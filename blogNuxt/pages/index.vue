52c24d8eab60b10cc1125e2ef33c1cf97e7ff212
Fernando Agostini <fernando.agostini.28@gmail.com>
2020-10-08 13:30:44 -0300

    Criando lebel sass

--- blogNuxt/pages/index.vue
+++ blogNuxt/pages/index.vue
@@ -1,20 +1,20 @@
-<template >
-  <div class = " container ">
+<template>
+  <div class="container">
     
-    <div class = " content ">
-      <h1 h1  class = " title ">
+    <div class="content">
+      <h1 h1 class="title">
         Novo Artigo
       </h1>
-      <form  name = " contato "  action = " "  method = " post ">
-        <label label  class = " form-label "  for = " nome ">
+      <form name="contato" action=" " method="post">
+        <label label class= "label-form" for="nome">
           Titulo:
-        </label >
-        <input  class = " textForm "  name = " name "  id = " name " />
-        <label label  class = " form-label "  for = " mensagem ">
+        </label>
+        <input class="textForm" name="name" id="name"/>
+        <label label class="label-form" for="mensagem">
           Mensagem:
         </label>
-        <textarea  class = " textForm "  name = " message "  id = " message " > </textarea >
-        <input  class = " buttonGreen "  type = " submit "  value = " Send message "/>
+        <textarea class="textForm" name="message"  id="message"> </textarea>
+        <input class="buttonGreen" type="submit" value="Send message"/>
       </form>
     </div>
   </div>
