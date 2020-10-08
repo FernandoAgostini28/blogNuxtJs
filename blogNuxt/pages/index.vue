6459ebe54195188548884bc502dde7d3cdb486ec
Fernando Agostini <fernando.agostini.28@gmail.com>
2020-10-08 16:22:57 -0300

    Criando metodos, funções  e cards

--- blogNuxt/pages/index.vue
+++ blogNuxt/pages/index.vue
@@ -5,27 +5,87 @@
       <h1 h1 class="titleForm">
         Novo Artigo
       </h1>
-      <form name="contato" action=" " method="post">
+      <form @submit.prevent="addArtigo">
         <label label class= "labelForm" for="nome">
           Titulo:
         </label>
-        <input class="textForm" name="name" id="name"/>
+        <input class="textForm" v-model="newTitulo" type="text" placeholder="Novo titulo" name=" newTitulo" id="newTitulo" value=""/>
         <label label class="labelForm" for="mensagem">
           Mensagem:
         </label>
-        <textarea class="textForm" name="message"  id="message"> </textarea>
+        <textarea class="textForm" v-model="newArtigo" type="text" placeholder="Nova Mensagem" name=" newArtigo" id="newArtigo" value=""> </textarea>
         <input class="buttonGreen" type="submit" value="Send message"/>
       </form>
+      
+    </div>
+    <div>
+      <ul> 
+      <!-- percorrendo o array com os artigos-->
+        <li class="contentCard" v-for=" (art , index) in artigos" :key="index"> 
+          <div class="row">
+            <div >
+              <div class="card-header"><h2 class="tituloArt" @click="exibirArtigo(index)">{{art.titulo}}</h2>
+              </div> 
+              <!-- Exibe o texto do artigo selecionado-->
+            <div class="card-body" v-if="artigoAberto === index" id="artigo">{{art.artigo}}</div>
+              <!-- botao para remover artigo -->
+              <button class="btn" @click="removeArt(art)" type="button">remove</button>
+            </div>
+          </div>   
+        </li>
+      </ul>
     </div>
   </div>
+  
+
+ 
+
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      titulo: '',
+        newTitulo: '',
+        newArtigo:'',
+        artigoAberto: null,
+        artigos: []
+     
+    };
+  },
+
+   /*metodo adiciona novo artigo ao Array*/
+    methods:{
+        addArtigo() {
+            this.artigos.push({
+                titulo: this.newTitulo,
+                artigo: this.newArtigo,
+                show: false,
+                
+            });
+            this.newTitulo = '';
+            this.newArtigo = '';
+        },
+        /* metodo para comparar  abrir artigo*/
+        exibirArtigo(index){
+          this.artigoAberto = index;
+        },
+        
+          
+            
+        
+        /* metodo para remover artigo*/
+        removeArt(art) {
+            const todoIndex = this.artigos.indexOf(art);
+            this.artigos.splice(todoIndex, 1);
+        }
+           
+        
+    }
+}
 </script>
 
-<style>
- 
-  
+<style scoped>
 
 </style>
